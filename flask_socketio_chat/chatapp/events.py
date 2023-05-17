fe2c4e25f2ac25a90561a58649ee58a8fb4cac05
Somaiya Albakhet <4111674@upm.edu.sa>
2023-05-18 01:19:05 +0300

    adding comments

--- flask_socketio_chat/chatapp/events.py
+++ flask_socketio_chat/chatapp/events.py
@@ -5,18 +5,18 @@ from .extensions import socketio
 
 users = {}
 
-
+# Function that will be called when the "connect" event is received from the client side
 @socketio.on("connect")
 def handle_connect():
     print("Client connected!")
 
-
+# Function that will be called when the "user_join" event is received from the client side
 @socketio.on("user_join")
 def handle_user_join(username):
     print(f"User {username} joined!")
     users[username] = request.sid
 
-
+# Function that will be called when the "new_message" event is received from the client side
 @socketio.on("new_message")
 def handle_new_message(message):
     print(f"New message: {message}")
@@ -26,7 +26,7 @@ def handle_new_message(message):
             username = user
     emit("chat", {"message": message, "username": username}, broadcast=True)
 
-
+# Function that will be called when the "image" event is received from the client side
 @socketio.on("image")
 def handle_image(image_data):
     print(f"Received Image data: {image_data}")
--- flask_socketio_chat/run.py
+++ flask_socketio_chat/run.py
@@ -3,3 +3,5 @@ from chatapp import create_app, socketio
 app = create_app()
 
 socketio.run(app)
+
+#app, port=443,8080
